3acd0ec0fde3e2ab28902987e1fdb49c2aec41bc
Yuhao-C <yuhaochen0107@gmail.com>
2022-07-18 11:30:04 -0400

    update edit docs link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,7 +36,8 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/Yuhao-C/gameboy-user-manual/",
+          editUrl:
+            "https://github.com/Yuhao-C/gameboy-user-manual/tree/master/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
